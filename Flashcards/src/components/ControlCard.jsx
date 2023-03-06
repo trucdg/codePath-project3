ebd7d17bac8bea9dec02156e7b64048968dc1b90
Truc Duong <trucdg@bu.edu>
2023-03-06 17:12:39 -0500

    add stylings to the card component

--- Flashcards/src/components/ControlCard.jsx
+++ Flashcards/src/components/ControlCard.jsx
@@ -20,6 +20,40 @@ const ControlCard = () => {
       answer:
         "Coupling refers to the degree of interdependence between software modules. High coupling means that modules are closely connected and changes in one module may affect other modules. Low coupling means that modules are independent and changes in one module have little impact on other modules.",
     },
+    {
+      question: "What are the various phases of SDLC?",
+      answer:
+        "- Requirement Analysis, Design, Develop, Testing and Maintenance ",
+    },
+    {
+      question: "What is Object Oriented-Programming (OOP)?",
+      answer:
+        "OOP is a “style” of programming characterized by the identification of classes of objects closely linked with the methods (functions) and attributes with which they are associated.",
+    },
+    {
+      question: "What are the 4 basics of OOP?",
+      answer: "Abstraction, Encapsulation, Inheritance, Polymorphism",
+    },
+    {
+      question: "What is 'Polymorphism' in OOP?",
+      answer:
+        "Polymorphism is an OOP concept that refers to the ability of objects to take on multiple form. It is the ability of a programming language to present the same interface for several different underlying data types.",
+    },
+    {
+      question: "What is 'Caching'?",
+      answer:
+        "Caching is a mechanism in which a subset of data is stored in a computer’s memory so that future requests for that data are served up faster than making costly requests to databases.",
+    },
+    {
+      question: "What is an Application Programming Interface (API)?",
+      answer:
+        "An API is a set of definitions and protocols to build a software and enable that software to communicate with different softwares using requests and responses.",
+    },
+    {
+      question: "What does CRUD stand for in programming?",
+      answer:
+        "CRUD stands for create, read, update and delete. They are the four basic operations of persistent data elements.",
+    },
   ];
 
   const [cardNum, setCardNum] = useState(cardLibrary[0]);
