18994cf91cb7d8552cefe3d204d005e87317c1bd
Truc Duong <trucdg@bu.edu>
2023-03-06 15:22:54 -0500

    move the library array from App.jsx to ControlCard component

--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -5,27 +5,6 @@ import QuestionCard from "./components/QuestionCard";
 import ControlCard from "./components/ControlCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // in the library array, the first component will be the start-intro card,
-  // the next cards will store question-answer pairs
-  let library = [
-    {
-      question: "Start!",
-      answer: "Press the arrow to start the flashcards :)",
-    },
-    {
-      question: "What is 'Encapsulation'?",
-      answer:
-        "Encapsulation is bundling data and methods that work on that data into one unit, like a class in Java. Encapsulation can also refer to a way to restrict the direct access to some components of an object.",
-    },
-    {
-      question: "What is 'Coupling' in software engineering?",
-      answer:
-        "Coupling refers to the degree of interdependence between software modules. High coupling means that modules are closely connected and changes in one module may affect other modules. Low coupling means that modules are independent and changes in one module have little impact on other modules.",
-    },
-  ];
-
   return (
     <div className="App">
       <header>
--- Flashcards/src/components/ControlCard.jsx
+++ Flashcards/src/components/ControlCard.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ControlCard = () => {
+  // in the library array, the first component will be the start-intro card,
+  // the next cards will store question-answer pairs
+  let library = [
+    {
+      question: "Start!",
+      answer: "Press the arrow to start the flashcards :)",
+    },
+    {
+      question: "What is 'Encapsulation'?",
+      answer:
+        "Encapsulation is bundling data and methods that work on that data into one unit, like a class in Java. Encapsulation can also refer to a way to restrict the direct access to some components of an object.",
+    },
+    {
+      question: "What is 'Coupling' in software engineering?",
+      answer:
+        "Coupling refers to the degree of interdependence between software modules. High coupling means that modules are closely connected and changes in one module may affect other modules. Low coupling means that modules are independent and changes in one module have little impact on other modules.",
+    },
+  ];
+
+  const [cardNum, setCardNum] = useState(library[0]);
   return <div>control card</div>;
 };
 
