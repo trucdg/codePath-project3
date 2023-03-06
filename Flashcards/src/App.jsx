5369954bc4ef7c294d32261ea68bccaf55ec0680
Truc Duong <trucdg@bu.edu>
2023-03-06 14:41:06 -0500

    add a hardcoded array to store 2 questions and their answers for the flashcard

--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -4,6 +4,18 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  let library = [
+    {
+      question: "What is 'Encapsulation'?",
+      answer:
+        "Encapsulation is bundling data and methods that work on that data into one unit, like a class in Java. Encapsulation can also refer to a way to restrict the direct access to some components of an object.",
+    },
+    {
+      question: "What is 'Coupling' in software engineering?",
+      answer:
+        "Coupling refers to the degree of interdependence between software modules. High coupling means that modules are closely connected and changes in one module may affect other modules. Low coupling means that modules are independent and changes in one module have little impact on other modules.",
+    },
+  ];
 
   return (
     <div className="App">
