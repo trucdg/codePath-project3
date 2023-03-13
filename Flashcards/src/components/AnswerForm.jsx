2ffdd251aa6c580aca4b49f8171adc3e582e23fe
Truc Duong <trucdg@bu.edu>
2023-03-13 18:46:13 -0400

    add a state variable to keep track of the entered input from the user for the answer text box

--- Flashcards/src/components/AnswerForm.jsx
+++ Flashcards/src/components/AnswerForm.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AnswerForm = () => {
+  const [answer, setAnswer] = useState("");
+
   const submitHandler = (event) => {
     event.preventDefault();
     console.log("form submitted!");
+    console.log(event.target.value);
+  };
+
+  const answerHandler = (event) => {
+    setAnswer(event.target.value);
+    console.log(answer);
   };
   return (
     <form onSubmit={submitHandler}>
       <label>
         Enter Your Guess
-        <input type="text" name="answer" />
+        <input
+          type="text"
+          placeholder="Your Answer"
+          onChange={answerHandler}
+          value={answer}
+        />
       </label>
       <button type="submit">Submit Guess</button>
     </form>
